5ee915c3b528e7b61dd36afc3141e6d86d017fd8
mycowbay777 <a1145509@mail.nuk.edu.tw>
2026-03-05 17:26:03 +0000

    2026/03/06

---
+++ Problem/UVa_299.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int t,x;
+    int arr[50];
+    cin>>t;
+    for(int i=0;i<t;i++){
+        int longg;
+        int count =0;
+        cin>>longg;
+       for(int k=0;k<longg;k++){
+        cin>>arr[k];
+       }
+       for(int k=0;k<longg-1;k++){
+        for(int i=0;i<longg-1-k;i++){
+            if(arr[i]>arr[i+1]){
+                swap(arr[i],arr[i+1]);
+                count++;
+            }
+        }
+        }
+        cout<<"Optimal train swapping takes "<<count<<" swaps"<<endl;
+    }
+}
