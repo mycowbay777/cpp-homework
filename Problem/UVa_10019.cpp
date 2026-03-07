c4dd6dc3730e825f91ae76863b64949af064b244
mycowbay777 <a1145509@mail.nuk.edu.tw>
2026-03-07 16:47:01 +0000

    2026/3/8

---
+++ Problem/UVa_10019.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    int arr[14]={1}; //2^14=16384可以覆蓋9999
+    cin>>t;
+    for(int k=1;k<14;k++){
+        arr[k]= arr[k-1]*2;
+    }
+    for(int i=0;i<t;i++){
+        int n;
+        int num;
+        int count1=0;
+        int count2=0;
+        cin>>n;
+        int temp = n;
+        for(int k=10;k>=0;k--){
+            if(n-arr[k]>=0){
+                n = n-arr[k];
+                count1++;
+            }
+        }
+        while(temp!=0) {
+            num = temp%10;
+            temp=temp/10;
+            for(int k=10;k>=0;k--){
+                if(num-arr[k]>=0){
+                    num = num-arr[k];
+                    count2++;
+                }
+            }
+        }
+        cout<<count1<<" "<<count2<<endl;
+    }
+}
