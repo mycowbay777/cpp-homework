d14b075e2d35610e0c2dd3830308ac26fc0f7718
mycowbay777 <a1145509@mail.nuk.edu.tw>
2026-03-09 11:43:26 +0000

    2026/3/9

---
+++ Problem/UVa_10050.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main(){
+
+    //用0 1陣列紀錄有無重複算到的罷工天
+
+    //再把所有固定放假的兩天扣掉 陣列中除7餘5和餘6的數
+
+    int t,n,p,day;
+
+    cin>>t;
+
+    while(t--){
+        int count =0;
+        int base[3700]={};
+        cin>>n>>p;
+        for(int i=0;i<p;i++){
+            cin>>day;
+            int gap = day;
+            for(int k=0;k<n/gap;k++){
+                if(!(day % 7 == 0 || day % 7 == 6)){
+                    base[day] = 1;
+                }
+                day+=gap;
+            }
+        }
+        for(int i=1;i<=n;i++){
+            if(base[i]==1){
+                count++;
+            }
+        }
+        cout<<count<<'\n';
+    }
+}
