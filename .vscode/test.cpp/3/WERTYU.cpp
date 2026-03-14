8b8a1e3e327cf2215e1d98915c77e074d5f5127a
mycowbay777 <a1145509@mail.nuk.edu.tw>
2026-03-14 07:08:12 +0000

    2026/3/14

--- .vscode/test.cpp/3/WERTYU.cpp
+++ .vscode/test.cpp/3/WERTYU.cpp
@@ -8,21 +8,24 @@ int main(){
     string enter;
     while(getline(cin,enter)){
         for(int i=0;i<enter.length();i++){
-            enter[i]==toupper(enter[i]);
+            enter[i] = toupper(enter[i]);
         }
          for(int i=0;i<enter.length();i++){
             if(enter[i]==' '){
                     cout<<' ';
                 }
-            for(int k=0;k<48;k++){
-                if(enter[i]=='Q'||enter[i]=='A'||enter[i]=='Z'){
-                    cout<<enter[i];
-                }
-                if(enter[i]==keyboard[k]){
-                    cout<<keyboard[k-1];
-                }
+            if(enter[i]=='Q'||enter[i]=='A'||enter[i]=='Z'){
+                cout<<enter[i];
+            }
+            else{
+                for(int k=0;k<48;k++){
+                    if(enter[i]==keyboard[k]){
+                        cout<<keyboard[k-1];
+                    }   
+                } 
             }
         }
+        cout<<'\n';
     }
     
 }
