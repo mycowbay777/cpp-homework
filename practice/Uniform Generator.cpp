ed3866d89ee3ef16e81a867ed104dda202a4aaef
mycowbay777 <a1145509@mail.nuk.edu.tw>
2026-03-16 09:50:20 +0000

    2026/3/16

--- practice/Uniform Generator.cpp
+++ practice/Uniform Generator.cpp
@@ -1,6 +1,26 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
+
 int main(){
+    int step;
+    int mod;
+    int seed=0;
+    while(cin>>step>>mod){
+        int count=1;
+        for(int i=0;i<mod;i++){
+            seed = (seed+step)%mod;
+             if(seed!=0){
+                count++;
+             }
+        }
+        cout<<setw(10)<<step<<setw(10)<<mod;
+        if(count==mod){
+            cout<<'     '<<"Good Choice";
+        }
+        else{
+            cout<<'     '<<"Bad Choice";
+        }
+    }
     
 }
