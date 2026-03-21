15a230c6aa2bec27bfa512e24a42b905d59479ea
mycowbay777 <a1145509@mail.nuk.edu.tw>
2026-03-21 09:58:27 +0000

    2026/3/21

--- Problem/UVa_10057.cpp
+++ Problem/UVa_10057.cpp
@@ -3,35 +3,33 @@ using namespace std;
 
 int main(){
     int n;
-    int mid;
+    long long int mid1;
+    long long int mid2;
     while(cin>>n){
-        int arr[1000000];
-        int count =0;
+        long long int arr[1000005];
+        long long int count =0;
         for(int i=0;i<n;i++){
             cin>>arr[i];
         }
+        sort(arr,arr+n);
         if(n%2==1){
-            mid = arr[n/2];
+            mid1 = arr[n/2];
             for(int i=0;i<n;i++){
-                if(mid==arr[i]){
+                if(mid1==arr[i]){
                     count++;
                 }
             }
-            cout<<mid<<" "<<count<<" 1";
+            cout<<mid1<<" "<<count<<" 1"<<endl;
         }
         else{
-            mid = arr[n/2-1];
+            mid1 = arr[n/2-1];
+            mid2 = arr[n/2];
             for(int i=0;i<n;i++){
-                if(mid==arr[i]){
+                if(mid1==arr[i]||mid2==arr[i]){
                     count++;
                 }
             }
-            if(mid==arr[n/2]){
-                cout<<mid<<" "<<count<<" 1"<<endl;
-            }
-            else{
-                cout<<mid<<" 2"<<" "<<arr[n/2]-mid+1<<endl;
-            }
+            cout<<mid1<<" "<<count<<" "<<(mid2-mid1+1)<<endl;
         }
     }
 }
