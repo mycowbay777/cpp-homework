df72d932289e735902166faba0259efab9caf641
mycowbay777 <a1145509@mail.nuk.edu.tw>
2026-03-21 09:32:07 +0000

    2026/3/21

--- Problem/UVa_10057.cpp
+++ Problem/UVa_10057.cpp
@@ -3,11 +3,35 @@ using namespace std;
 
 int main(){
     int n;
-    int num;
+    int mid;
     while(cin>>n){
         int arr[1000000];
+        int count =0;
         for(int i=0;i<n;i++){
-            cin>>num;
+            cin>>arr[i];
+        }
+        if(n%2==1){
+            mid = arr[n/2];
+            for(int i=0;i<n;i++){
+                if(mid==arr[i]){
+                    count++;
+                }
+            }
+            cout<<mid<<" "<<count<<" 1";
+        }
+        else{
+            mid = arr[n/2-1];
+            for(int i=0;i<n;i++){
+                if(mid==arr[i]){
+                    count++;
+                }
+            }
+            if(mid==arr[n/2]){
+                cout<<mid<<" "<<count<<" 1"<<endl;
+            }
+            else{
+                cout<<mid<<" 2"<<" "<<arr[n/2]-mid+1<<endl;
+            }
         }
     }
 }
