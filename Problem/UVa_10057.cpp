bd636a0fd6cfbdb1825f46e9200f681c9cd87a74
mycowbay777 <a1145509@mail.nuk.edu.tw>
2026-03-21 08:21:07 +0000

    2026/3/21

---
+++ Problem/UVa_10057.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    int num;
+    while(cin>>n){
+        int arr[1000000];
+        for(int i=0;i<n;i++){
+            cin>>num;
+        }
+    }
+}
