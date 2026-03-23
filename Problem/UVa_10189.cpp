e3a12ca74c48a313095f1f4960492ad53126eb50
mycowbay777 <a1145509@mail.nuk.edu.tw>
2026-03-23 10:32:37 +0000

    2026/3/21

--- Problem/UVa_10189.cpp
+++ Problem/UVa_10189.cpp
@@ -1,17 +1,47 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int dx[]={-1,-1,-1,0,0,1,1,1};
+int dy[]={1,0,-1,1,-1,1,0,-1};
+
 int main(){
     int x,y;
-    string s[105][105];
+    int filenum=1;
     while(cin>>x>>y){
+        if(filenum!=1){
+            cout<<endl;
+        }
+        if(x==0&&y==0){
+            return 0;
+        }
+        vector<string> s(x);
+        vector<vector<int>> mmm(x,vector<int>(y,0));
         for(int i=0;i<x;i++){
+            cin>>s[i];
             for(int j=0;j<y;j++){
-                cin>>s[x][y];
+                if(s[i][j]=='*'){
+                    for(int k=0;k<8;k++){
+                        int nx=i+dx[k];
+                        int ny=j+dy[k];
+                        if(nx>=0&&nx<x&&ny>=0&&ny<y){
+                            mmm[nx][ny]++;
+                        }
+                    }
+                }
             }
         }
-        for(int i=0;i<8;i++){
-            
+        cout<<"Field #"<<filenum<<":"<<endl;
+        filenum++;
+        for(int i=0;i<x;i++){
+            for(int j=0;j<y;j++){
+                if(s[i][j]=='*'){
+                    cout<<"*";
+                }
+                else{
+                    cout<<mmm[i][j];
+                }
+            }
+            cout<<endl;
         }
     }
 }
