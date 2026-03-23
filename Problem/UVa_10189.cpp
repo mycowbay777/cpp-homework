5a48451ae391bdba759dbfdbaef9ff6116f82ab6
mycowbay777 <a1145509@mail.nuk.edu.tw>
2026-03-23 07:10:31 +0000

    2026/3/21

---
+++ Problem/UVa_10189.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x,y;
+    string s[105][105];
+    while(cin>>x>>y){
+        for(int i=0;i<x;i++){
+            for(int j=0;j<y;j++){
+                cin>>s[x][y];
+            }
+        }
+        for(int i=0;i<8;i++){
+            
+        }
+    }
+}
