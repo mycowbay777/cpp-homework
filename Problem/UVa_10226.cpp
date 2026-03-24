9a87f178ae18db0f27f161647bfbbd3ed617a94b
mycowbay777 <a1145509@mail.nuk.edu.tw>
2026-03-24 02:06:44 +0000

    2026/3/21

---
+++ Problem/UVa_10226.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    int first=1;
+    cin>>t;
+    if(first!=1){
+        cout<<endl;
+    }
+    string tree;
+    vector<string> tree_sum;
+    vector<double> counter={0.0};
+}
