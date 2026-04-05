2b9d7cc4366556a0c300fcf3b34d615c2df880a4
mycowbay777 <a1145509@mail.nuk.edu.tw>
2026-04-05 15:22:01 +0000

    2026/04/05

--- practice/20260331C.cpp
+++ practice/20260331C.cpp
@@ -1,27 +1,39 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<string>
 using namespace std;
 
 int main(){
     string move;
-    vector<string> website;
+    vector<string> website(105);
     website[0] ={"http://www.acm.org/"};
-    int website_num=1;
-    while(cin>>move){
+    int website_num=0;
+    int max_website_num=0;
+    while(cin>>move&&move!="QUIT"){
         if(move=="VISIT"){
-            cin>>website[website_num];
-            cout<<website[website_num];
             website_num++;
+            max_website_num = website_num;
+            cin>>website[website_num];
+            cout<<website[website_num]<<endl;
         }
         else if(move=="BACK"){
             if(website_num-1<0){
-                cout<<"Ignored";
+                cout<<"Ignored"<<endl;
             }
             else{
-                cout<<website[website_num-1];
+                website_num-=1;
+                cout<<website[website_num]<<endl;
             }
         }
         else if(move=="FORWARD"){
-
+            if(website_num+1>max_website_num){
+                cout<<"Ignored"<<endl;
+            }
+            else{
+                website_num+=1;
+                cout<<website[website_num]<<endl;
+            }
         }
     }
+    return 0;
 }
