8da36693724897b787e1996f43b118e18ecfc67d
mycowbay777 <a1145509@mail.nuk.edu.tw>
2026-04-11 07:08:17 +0000

    2026/04/11

--- practice/20260407A.cpp
+++ practice/20260407A.cpp
@@ -1,21 +1,42 @@
 #include<bits/stdc++.h>
+#include<stack> //後進先出
 using namespace std;
 
 int main(){
     int n;
-    int arr[1005];
-    while(cin>>n){
-        if(n==0){
-            return 0;
-        }
+    while(cin>>n&&n!=0){
         while(1){
-            cin>>arr[0];
-            if(arr[0]==0){
+            vector<int> target(n);
+            cin>>target[0];
+            if(target[0]==0){
                 break;
             }
             for(int i=1;i<n;i++){
-                cin>>arr[i];
+                cin>>target[i];
             }
-        }
+            vector<int> station;
+            int current_train=1;
+            bool possible = true;
+            for(int i=0;i<n;i++){
+                while(current_train<=n&&(station.empty()||station.back()!=target[i])){ //如果車站頂端不是我們要的，且還有車廂在 A 方向，就一直進站
+                    station.push_back(current_train);
+                    current_train++;
+                }
+                if(!station.empty()&&station.back()==target[i]){
+                    station.pop_back();
+                }
+                else{
+                    possible = false;
+                    break;
+                }
+            }
+            if(possible){
+                cout<<"Yes"<<endl;
+            }
+            else{
+                cout<<"No"<<endl;
+            }
+        }    
+        cout<<endl;
     }
 }
