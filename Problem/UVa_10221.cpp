43157c0731f3ce9545f0a7f725d6f8e4e592ab3e
mycowbay777 <a1145509@mail.nuk.edu.tw>
2026-04-28 02:26:03 +0000

    20260428

--- Problem/UVa_10221.cpp
+++ Problem/UVa_10221.cpp
@@ -1,6 +1,22 @@
 #include<bits/stdc++.h>
+#include<cmath>
 using namespace std;
+#define pi asin(1.0)*2
 
 int main(){
-    double s,a;
+    double s,a,r;
+    string unit;
+    double arc,chord;
+    r=6440.0;
+    while(cin>>s>>a>>unit){
+        if(unit=="min"){
+            a/=60;
+        }
+        if(a>180.0){
+            a=360.0-a;
+        }
+        arc=2.0*pi*(r+s)*a/360;
+        chord=2.0*(r+s)*sin(a/2*pi/180); //要換算成弧長
+        cout<<fixed<<setprecision(6)<<arc<<" "<<chord<<"\n";
+    }
 }
