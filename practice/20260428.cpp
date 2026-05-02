9b51e46c66e5b8055d9e2675b22ff7450e28145c
mycowbay777 <a1145509@mail.nuk.edu.tw>
2026-05-02 08:05:13 +0000

    20260502

---
+++ practice/20260428.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int N,W;
+    cin>>N>>W;
+    vector<long long> gg;
+    for(int i=0;i<N;i++){
+        int n,w;
+        cin>>n>>w;
+        
+    }
+    
+}
